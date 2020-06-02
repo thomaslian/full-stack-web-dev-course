3c91203d49f23efcedb7414c43d13e5e95a13114
Thomas Oedegaard <thomaslio@live.no>
2020-06-02 16:22:59 +0200

    Express example

--- NodeJS/README.md
+++ NodeJS/README.md
@@ -6,10 +6,18 @@ Link: https://www.coursera.org/learn/server-side-nodejs
 ## npm CLI commands
 - npm init (Initialize this folder as a node folder)
 - npm start (Start the application that is in the folder)
+- npm install express@4.0.0 (Install exact version) 
+- npm install express@"~4.0.0" (Install and accept version with patch) 
+- npm install express@"^4.0.0" (Install and acceptminor version) 
+- "--save", saves the module in the dependencies list
 
 ## npm CLI
 - v. 6.14.5
 
+## Node modules for server development
+- Express
+
 ## Keywords
 - NodeJS
 - Postman
+- Express
---
+++ NodeJS/node-express/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const http = require('http');
+
+const hostname = 'localhost';
+const port = 3000;
+
+const app = express();
+
+// req = request
+// res = result
+// next = Is used when you need to invoke additional 
+// middleware to take care of work on your behalf.
+// Next is optional
+app.use((req, res, next) => {
+    console.log(req.headers);
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<html><body><h1>This is an Express server</h1></body></html>');
+});
+
+const server = http.createServer(app);
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+  });
