b8028ca31a1b0644d2766e6043fb3119cf7c4a9f
Thomas Oedegaard <thomaslio@live.no>
2020-06-02 18:34:51 +0200

    Node: Express router

--- NodeJS/node-express/index.js
+++ NodeJS/node-express/index.js
@@ -3,6 +3,8 @@ const http = require('http');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
+const dishRouter = require('./routes/dishRouter');
+
 const hostname = 'localhost';
 const port = 3000;
 
@@ -11,60 +13,8 @@ app.use(morgan('dev'));
 // Allows us to parse the body of the request messages which is formatted in json
 app.use(bodyParser.json());
 
-app.all('/dishes', (req,res,next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type','text/plain');
-
-    // Contine to look for additional specifications below which will 
-    // match the dishes endpoint. Example a app.get('/dishes') if it is
-    // one below. All properties will be the same as the ones that 
-    // are set in this one.
-    next();
-});
-
-// Get dishes
-app.get('/dishes', (req,res,next) => {
-    res.end('Will send all the dishes to you!');
-})
-
-// Post new dish
-app.post('/dishes', (req,res,next) => {
-    res.end('Will add the dish: ' + req.body.name + ' with details: ' + req.body.description);
-});
-
-// Update an existing dish (not supported)
-app.put('/dishes', (req,res,next) => {
-    res.statusCode = 403;
-    res.end('PUT operation not supported on /dishes');
-});
-
-// Delete dishes
-app.delete('/dishes', (req,res,next) => {
-    res.end('Deleting all the dishes!');
-})
-
-// Get dish with ID
-app.get('/dishes/:dishId', (req,res,next) => {
-    res.end('Will send details of the dish: ' + req.params.dishId + ' to you!');
-})
-
-// Post new dish with ID (not supported)
-app.post('/dishes/:dishId', (req,res,next) => {
-    res.statusCode = 403;
-    res.end('POST operation not supported on /dishes/'+ req.params.dishId);
-});
-
-// Update an existing dish with ID
-app.put('/dishes/:dishId', (req,res,next) => {
-    res.write('Updating the dish: ' + req.params.dishId + '\n');
-    res.end('Will update the dish: ' + req.body.name + " with details: " + req.body.description);
-});
-
-// Delete dish with ID
-app.delete('/dishes/:dishId', (req,res,next) => {
-    res.end('Deleting dish: ' + req.params.dishId);
-});
-
+// Mount the dishRouter, and it will use the link '/dishes'
+app.use('/dishes', dishRouter);
 
 // Look at the this folder (public folder) to serve static html files
 app.use(express.static(__dirname+ '/public'));
